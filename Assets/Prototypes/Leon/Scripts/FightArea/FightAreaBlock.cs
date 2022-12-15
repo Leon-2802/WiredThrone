432d244eaddc6ce72f8c5d2048bc00cc6b32358d
Leon_2802 <leon.gobbert@hs-furtwangen.de>
2022-12-15 17:52:52 +0100

    fight area improved get position

--- Assets/Prototypes/Leon/Scripts/FightArea/FightAreaBlock.cs
+++ Assets/Prototypes/Leon/Scripts/FightArea/FightAreaBlock.cs
@@ -4,9 +4,14 @@ public class FightAreaBlock : MonoBehaviour
 {
     [SerializeField] private Vector2 coordinates;
 
+    private void Start() 
+    {
+        coordinates = new Vector2(transform.position.x, this.gameObject.GetComponentInParent<Transform>().position.z);
+    }
+
     private void OnTriggerEnter(Collider other) 
     {
-        if(other.gameObject.GetComponent<MovingCharacter>())
+        if(other.gameObject.GetComponent<Robot>())
         {
             ManagePositions.instance.robotPosition = coordinates;
         }
